5274031752bc4c30feee1bb057450dae7965fe0a
Mswork6 <denis.ch74@mail.ru>
2024-10-16 00:24:15 +0500

    feat: made dayitem animation

--- app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/DayItem.kt
+++ app/src/main/java/com/example/courseworkandroidweeklyplanner/presentation/screens/main/DayItem.kt
@@ -1,6 +1,7 @@
 package com.example.courseworkandroidweeklyplanner.presentation.screens.main
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,8 +13,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,36 +33,45 @@ fun DayItem(
     day: Day,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
-) = Card(
-    shape = MaterialTheme.shapes.large,
-    colors = CardDefaults.cardColors(
-        containerColor = MaterialTheme.colorScheme.primary
-    ),
-    elevation = CardDefaults.cardElevation(2.dp),
-    onClick = onClick,
-    modifier = modifier
 ) {
-    Row(
-        modifier = Modifier
-            .padding(vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
+    val rotationState by animateFloatAsState(
+        targetValue = if(day.isExpanded) 180f else 0f,
+        label = stringResource(R.string.descipition_dayitem_animation)
+    )
+
+    Card(
+        shape = MaterialTheme.shapes.large,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        elevation = CardDefaults.cardElevation(2.dp),
+        onClick = onClick,
+        modifier = modifier
     ) {
-        Text(
+        Row(
             modifier = Modifier
-                .weight(2f)
-                .padding(start = 16.dp),
-            text = day.name,
-            fontWeight = FontWeight.SemiBold
-        )
-        Text(
-            modifier = Modifier.weight(2f),
-            text = dateToString(day.date),
-        )
-        Icon(
-            modifier = Modifier.weight(1f),
-            imageVector = Icons.Default.KeyboardArrowDown,
-            contentDescription = stringResource(R.string.description_show_hide_tasks)
-        )
+                .padding(vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(start = 16.dp),
+                text = day.name,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(
+                modifier = Modifier.weight(2f),
+                text = dateToString(day.date),
+            )
+            Icon(
+                modifier = Modifier
+                    .weight(1f)
+                    .rotate(rotationState),
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = stringResource(R.string.description_show_hide_tasks)
+            )
+        }
     }
 }
 
